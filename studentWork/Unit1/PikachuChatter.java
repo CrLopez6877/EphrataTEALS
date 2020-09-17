553938c63022e23012349a642663c6f6fe8f01fd
CrLopez6877 <CrLopez6877@outlook.com>
2020-09-17 21:52:54 +0000

    CrLopez6877

---
+++ studentWork/Unit1/PikachuChatter.java
@@ -0,0 +1,19 @@
+package studentWork.Unit1;
+
+public class PikachuChatter {
+    public static void main (String[] args) {
+        System.out.println("Pika pika pika chu pika chu peeeee ka pika chu!");
+        System.out.println("(\\__/)");
+        System.out.println("(o^.^)");
+        System.out.println("z(_(\")(\")");
+        System.out.println("Pika? Pika pika pika chu peeeee ka chu!");
+        System.out.println("(\\__/)");
+        System.out.println("(o^.^)");
+        System.out.println("z(_(\")(\")");
+        
+
+
+	}
+
+    
+}
